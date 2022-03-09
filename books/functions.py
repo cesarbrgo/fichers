ed2b658cbb87720d3eda1032408d66b63de7d9e7
cesarbrgo <95416846+cesarbrgo@users.noreply.github.com>
2022-03-09 14:23:51 +0100

    ejercicio 9

--- books/functions.py
+++ books/functions.py
@@ -1,38 +1,57 @@
 import csv
-def insert_data():
-    book = dict()
-    book['isbn'] = input("Introdueix l'isbn del llibre: ")
-    book['title'] = input("Introdueix el títol del llibre: ")
-    book['author'] = input("Introdueix l'autor del llibre: ")
-    book['editorial'] = input("Introdueix l'editorial del llibre: ")
-    book['pub_date'] = input("Introdueix la data de publicació del llibre: ")
-    insert_book(book)
+import literals as lit
+import datetime
 
-def insert_book(book):
-    path='files/books.csv'
+def insert_data():
+    video = dict()
+    video['identificador'] = int(input(lit.msg1))
+    video['grup'] = input(lit.msg2)
+    video['canço'] = input(lit.msg3)
+    dia=int(input(lit.dies))
+    while dia<1 or dia>30:
+        dia = int(input(lit.dies))
+    mes=int(input(lit.mesos))
+    while mes<1 or mes>12:
+        mes = int(input(lit.mesos))
+    any=int(input(lit.anys))
+    currentDateTime = datetime.datetime.now()
+    date = currentDateTime.date()
+    print(date)
+    year = date.strftime("%Y")
+    year=int(year)
+    while any<1970 or any> year:
+        any = int(input(lit.anys))
+    video['data'] = str(dia)+'/'+str(mes)+'/'+str(any)
+    video['nombrevisua'] = int(input(lit.visualitzacions))
+    while video['nombrevisua']<1:
+        video['nombrevisua'] = int(input(lit.visualitzacions))
+    insert_video(video)
+
+def insert_video(video):
+    path='files/video.csv'
     header=existint_file(path)
-    with open('files/books.csv', 'a', encoding='utf-8', newline='') as csvfile:
-        fieldnames = ['isbn', 'title', 'author', 'editorial', 'pub_date']
-        writeCSV = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        try:
+
+    try:
+        with open('files/video.csv', 'a', encoding='utf-8', newline='') as csvfile:
+            fieldnames = ['identificador', 'grup', 'canço', 'data', 'nombrevisua']
+            writeCSV = csv.DictWriter(csvfile, fieldnames=fieldnames)
             if header==0:
-                writeCSV.writeheader()
-            writeCSV.writerow(book)
-        except:
-            print("No s'ha pogut inserir el registre.")
-        else:
-            print("El registre s'ha inserit correctament.")
+                    print(lit.crea)
+                    writeCSV.writeheader()
+            writeCSV.writerow(video)
+    except:
+        print(lit.noinserir)
+    else:
+        print(lit.inserir)
 
 def existint_file(path):
     try:
         f = open(path, "r")
     except FileNotFoundError:
-        print("No sha trobat el fitxer")
+        print(lit.noexiste)
         header=0
     else:
          header=1
          f.close()
     finally:
         return header
-
-
