e5b60058df6c2b71a633d034b447c4f00a759841
cesarbrgo <95416846+cesarbrgo@users.noreply.github.com>
2022-03-10 13:02:21 +0100

    Add files via upload

--- books/functions.py
+++ books/functions.py
@@ -2,56 +2,13 @@ import csv
 import literals as lit
 import datetime
 
-def insert_data():
-    video = dict()
-    video['identificador'] = int(input(lit.msg1))
-    video['grup'] = input(lit.msg2)
-    video['canço'] = input(lit.msg3)
-    dia=int(input(lit.dies))
-    while dia<1 or dia>30:
-        dia = int(input(lit.dies))
-    mes=int(input(lit.mesos))
-    while mes<1 or mes>12:
-        mes = int(input(lit.mesos))
-    any=int(input(lit.anys))
-    currentDateTime = datetime.datetime.now()
-    date = currentDateTime.date()
-    print(date)
-    year = date.strftime("%Y")
-    year=int(year)
-    while any<1970 or any> year:
-        any = int(input(lit.anys))
-    video['data'] = str(dia)+'/'+str(mes)+'/'+str(any)
-    video['nombrevisua'] = int(input(lit.visualitzacions))
-    while video['nombrevisua']<1:
-        video['nombrevisua'] = int(input(lit.visualitzacions))
-    insert_video(video)
-
-def insert_video(video):
-    path='files/video.csv'
-    header=existint_file(path)
-
-    try:
-        with open('files/video.csv', 'a', encoding='utf-8', newline='') as csvfile:
-            fieldnames = ['identificador', 'grup', 'canço', 'data', 'nombrevisua']
-            writeCSV = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            if header==0:
-                    print(lit.crea)
-                    writeCSV.writeheader()
-            writeCSV.writerow(video)
-    except:
-        print(lit.noinserir)
-    else:
-        print(lit.inserir)
-
-def existint_file(path):
-    try:
-        f = open(path, "r")
-    except FileNotFoundError:
-        print(lit.noexiste)
-        header=0
-    else:
-         header=1
-         f.close()
-    finally:
-        return header
+def get_info():
+        line_count=0
+        total_woman=0
+        with open('files/Creus.csv') as csvfile:
+            readCSV=csv.DictReader(csvfile)
+            for row in readCSV:
+                if row['Sexe']=='Dona':
+                    total_woman+=1
+                line_count+=1
+        print("el total de dones es:", total_woman, 'el total de registres es: ', line_count)
--- books/main.py
+++ books/main.py
@@ -2,10 +2,7 @@ import csv
 import functions as fun
 
 def main():
-    stop=1
-    while stop!=0:
-        fun.insert_data()
-        stop=int(input("vols parar d'introduir registres? introdueix 0: "))
+    fun.get_info()
 
 if __name__ == '__main__':
     main()
